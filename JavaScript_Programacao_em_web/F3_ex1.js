fb6b10a8c5da276659ca70edc060758bafdce207
vladkk24 <vladkk24@gmail.com>
2019-10-17 09:44:15 +0100

    Novo CODIGOGKEGWOJGOEG

---
+++ JavaScript_Programacao_em_web/F3_ex1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var nome = prompt("Qual o sue nome", "Nome") ;
+//document.body.innerHTML="Programação em Web - Cliente";
+document.body.innerText("<h1>Programação em Web - Cliente</h1>");
+document.createElement("programacao");
+
+if (nome == null) {
+    alert("Não tem nome?");
+}
+else {
+    alert("Olá " + nome + " sejá bem vindo.");
+}
+
+
+
+
+
+
